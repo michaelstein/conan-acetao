c2f72d2675da5cc6ef0e74f4faf30df7943ba70d
mstein <michael@insanefactory.com>
2022-04-02 23:12:56 +0200

    Unify functions

--- conanfile.py
+++ conanfile.py
@@ -28,33 +28,7 @@ class AcetaoConan(ConanFile):
 		tools.get("https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%%2BTAO-%s/ACE+TAO-%s.tar.bz2" % (versionUnderscore, self.version))
 		tools.rename("ACE_wrappers", self._source_subfolder)
 
-	def _buildAutotoolsAce(self):
-		buildType = self.settings.get_safe("build_type", default="Release")
-
-		with open(os.path.join(self._source_subfolder, "ace", "config.h"), "w") as f:
-			f.write("#include \"ace/config-win32.h\"\n")
-
-		with open(os.path.join(self._source_subfolder, "include", "makeinclude", "platform_macros.GNU"), "w") as f:
-			f.write("buildbits=64\n")
-			f.write("debug=%s\n" % "1" if buildType == "Debug" else "0")
-			f.write("include $(ACE_ROOT)/include/makeinclude/platform_mingw32.GNU\n")
-			f.write("rand_r_issue=1\n")
-			f.write("INSTALL_PREFIX=%s\n" % tools.unix_path(os.path.join(self.build_folder, self._install_subfolder)))
-
-		args = ""
-		if not self.options.shared:
-			args = "static_libs_only=1"
-
-		with tools.chdir(os.path.join(self._source_subfolder, "ace")):
-			autotools = AutoToolsBuildEnvironment(self, win_bash=True)
-			
-			envVars = autotools.vars
-			envVars["ACE_ROOT"] = tools.unix_path(os.path.join(self.build_folder, self._source_subfolder))
-			
-			autotools.make(args=args, vars=envVars)
-			autotools.install(vars=envVars)
-
-	def _buildAutotoolsAce(self):
+	def _configureAutotools(self):
 		buildType = self.settings.get_safe("build_type", default="Release")
 
 		with open(os.path.join(self._source_subfolder, "ace", "config.h"), "w") as f:
@@ -63,47 +37,14 @@ class AcetaoConan(ConanFile):
 		with open(os.path.join(self._source_subfolder, "include", "makeinclude", "platform_macros.GNU"), "w") as f:
 			f.write("c++11=1\n")
 			f.write("buildbits=64\n")
+			if not self.options.shared:
+				f.write("static_libs_only=1\n")
 			f.write("debug=%s\n" % "1" if buildType == "Debug" else "0")
 			f.write("include $(ACE_ROOT)/include/makeinclude/platform_mingw32.GNU\n")
-			f.write("rand_r_issue=1\n")
 			f.write("INSTALL_PREFIX=%s\n" % tools.unix_path(os.path.join(self.build_folder, self._install_subfolder)))
 
-		args = ""
-		if not self.options.shared:
-			args = "static_libs_only=1"
-
-		with tools.chdir(os.path.join(self._source_subfolder, "ace")):
-			autotools = AutoToolsBuildEnvironment(self, win_bash=True)
-			
-			envVars = autotools.vars
-			envVars["ACE_ROOT"] = tools.unix_path(os.path.join(self.build_folder, self._source_subfolder))
-			
-			autotools.make(args=args, vars=envVars)
-			autotools.install(vars=envVars)
-
-	def _buildAutotoolsGperf(self):
-		with tools.chdir(os.path.join(self._source_subfolder, "apps", "gperf", "src")):
-			autotools = AutoToolsBuildEnvironment(self, win_bash=True)
-			
-			envVars = autotools.vars
-			envVars["ACE_ROOT"] = tools.unix_path(os.path.join(self.build_folder, self._source_subfolder))
-			
-			autotools.make(vars=envVars)
-			autotools.install(vars=envVars)
-
-	def _buildAutotoolsTaoIdl(self):
-		with tools.chdir(os.path.join(self._source_subfolder, "TAO", "TAO_IDL")):
-			autotools = AutoToolsBuildEnvironment(self, win_bash=True)
-			
-			envVars = autotools.vars
-			envVars["ACE_ROOT"] = tools.unix_path(os.path.join(self.build_folder, self._source_subfolder))
-			envVars["TAO_ROOT"] = tools.unix_path(os.path.join(self.build_folder, self._source_subfolder, "TAO"))
-			
-			autotools.make(vars=envVars)
-			autotools.install(vars=envVars)
-
-	def _buildAutotoolsTaoLib(self):
-		with tools.chdir(os.path.join(self._source_subfolder, "TAO", "tao")):
+	def _buildAutotoolsPath(self, path):
+		with tools.chdir(path):
 			autotools = AutoToolsBuildEnvironment(self, win_bash=True)
 			
 			envVars = autotools.vars
@@ -115,10 +56,11 @@ class AcetaoConan(ConanFile):
 
 	def build(self):
 		if self.settings.os == "Windows" and self.settings.compiler == "gcc":
-			self._buildAutotoolsAce()
-			self._buildAutotoolsGperf()
-			self._buildAutotoolsTaoIdl()
-			self._buildAutotoolsTaoLib()
+			self._configureAutotools()
+			self._buildAutotoolsPath(os.path.join(self._source_subfolder, "ace"))
+			self._buildAutotoolsPath(os.path.join(self._source_subfolder, "apps", "gperf", "src"))
+			self._buildAutotoolsPath(os.path.join(self._source_subfolder, "TAO", "TAO_IDL"))
+			self._buildAutotoolsPath(os.path.join(self._source_subfolder, "TAO", "tao"))
 
 	def package(self):
 		self.copy("*.h", dst="include", src="hello")
